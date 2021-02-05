bb8766ab215d023ec9e40d08e7e98575108b91f1
Andrea Bigagli <abigagli@gmail.com>
2021-02-05 16:44:03 +0100

    Refactor to use a modbus_t context wrapper

--- src/context.h
+++ src/context.h
@@ -3,15 +3,140 @@
 #include <modbus.h>
 #include <memory>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <tuple>
+#include <cassert>
 
 namespace modbus {
+using namespace std::string_literals;
+class SerialLine
+{
+    friend class RTUContext;
+    std::string device_;
+    int bps_;
+    int data_bits_;
+    char parity_;
+    int stop_bits_;
 
-    class Context
+    auto unpack_line_config(std::istringstream iss)
     {
-        std::unique_ptr<modbus_t> ctx_;
+        std::vector<std::string> parts;
+        std::string elem;
+        while (std::getline(iss, elem, ':'))
+        {
+            parts.push_back(elem);
+        }
 
-        public:
-        Context ()
+        if (parts.size() != 4)
+            throw std::runtime_error("Invalid line config: " + iss.str());
+
+        return std::tuple<int, int, char, int>{std::stoi(parts[0]),
+                                               std::stoi(parts[1]),
+                                               parts[2][0],
+                                               std::stoi(parts[3])};
     }
 
-}// namespace modbus
+public:
+    SerialLine(std::string device, std::string const &line_config)
+      : device_(device)
+    {
+        std::tie(bps_, data_bits_, parity_, stop_bits_) =
+          unpack_line_config(std::istringstream(line_config));
+    }
+    };
+    class RTUContext
+    {
+        struct ctx_deleter
+        {
+            void operator()(modbus_t *ctx)
+            {
+                modbus_close(ctx);
+                modbus_free(ctx);
+            }
+        };
+
+        std::unique_ptr<modbus_t, ctx_deleter> ctx_;
+
+    public:
+        RTUContext(int server_id,
+                   SerialLine const &serial_line,
+                   std::chrono::milliseconds const &answering_time,
+                   bool verbose = false)
+        {
+            ctx_.reset(modbus_new_rtu(serial_line.device_.c_str(),
+                                      serial_line.bps_,
+                                      serial_line.parity_,
+                                      serial_line.data_bits_,
+                                      serial_line.stop_bits_));
+
+            if (!ctx_)
+                throw std::runtime_error("Failed creating ctx for device " +
+                                         serial_line.device_);
+
+            int api_rv;
+            if (verbose)
+                api_rv = modbus_set_debug(ctx_.get(), TRUE);
+
+            api_rv = modbus_set_error_recovery(
+              ctx_.get(),
+              static_cast<modbus_error_recovery_mode>(
+                MODBUS_ERROR_RECOVERY_LINK | MODBUS_ERROR_RECOVERY_PROTOCOL));
+
+            auto seconds =
+              std::chrono::duration_cast<std::chrono::seconds>(answering_time);
+            auto microseconds =
+              std::chrono::microseconds(answering_time - seconds);
+
+            modbus_set_response_timeout(
+              ctx_.get(), seconds.count(), microseconds.count());
+            modbus_set_slave(ctx_.get(), server_id);
+
+            if (modbus_connect(ctx_.get()) < 0)
+                throw std::runtime_error("Failed modbus_connect: "s +
+                                         modbus_strerror(errno));
+        }
+
+        int64_t read_holding_registers(int address, int regsize)
+        {
+            uint16_t regs[4];
+            int api_rv = modbus_read_registers(ctx_.get(),
+                                           address,
+                                           regsize,
+                                           regs); // Holding register: Code 03
+
+            if (api_rv != regsize)
+                throw std::runtime_error("Failed modbus_read_registers: "s +
+                                         modbus_strerror(errno));
+
+            // api_rv = modbus_read_input_registers(ctx, options::address,
+            // options::regsize, regs);    // Input register: Code 04
+
+            int64_t val;
+            switch (regsize)
+            {
+            case 1:
+                val = regs[0];
+                break;
+            case 2:
+                val = MODBUS_GET_INT32_FROM_INT16(regs, 0);
+                break;
+            case 4:
+                val = MODBUS_GET_INT64_FROM_INT16(regs, 0);
+                break;
+            default:
+                assert(!"Unreachable");
+            }
+
+            return val;
+        }
+
+        template <class F, class... Args>
+        auto call(F &&callable, Args &&...args)
+        {
+            return std::invoke(std::forward<F>(callable),
+                               ctx_.get(),
+                               std::forward<Args>(args)...);
+        }
+    };
+} // namespace modbus
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
-#include <modbus.h>
+#include "context.h"
+
 #include <nlohmann/json.hpp>
 
 #include <iostream>
 #include <string>
-#include <vector>
-#include <sstream>
 #include <chrono>
 #include <unistd.h>
 
-using namespace std::string_literals;
 using namespace std::chrono_literals;
 using json = nlohmann::json;
 
-namespace
+namespace {
+
+int
+usage(std::string const &prog_name, int res, std::string const &msg = "")
 {
-    int usage (std::string const &prog_name, int res, std::string const &msg = "")
-    {
-        if (!msg.empty())
-            std::cout << "\n*** ERROR: " << msg << " ***\n\n";
-        std::cout << "Usage:\n";
-        std::cout << prog_name << R"(
+    if (!msg.empty())
+        std::cout << "\n*** ERROR: " << msg << " ***\n\n";
+    std::cout << "Usage:\n";
+    std::cout << prog_name << R"(
                 [-h(help)]
                 [-d <device = /dev/ttyUSB0>]
                 [-v(erbose)]
@@ -27,26 +26,11 @@ namespace
                 [-a <answering_timeout_ms =500>]
                 -s <server_id>
                 <regnum>
-                <regsize ={1|2|4}>)" << std::endl;
-        return res;
-    }
-
-    auto unpack_line_config (std::istringstream iss)
-    {
-        std::vector<std::string> parts;
-        std::string elem;
-        while (std::getline(iss, elem, ':'))
-        {
-            parts.push_back(elem);
-        }
-
-        if (parts.size() != 4)
-            throw std::runtime_error("Invalid line config: " + iss.str());
-
-        return std::tuple<int, int, char, int>{std::stoi(parts[0]), std::stoi(parts[1]), parts[2][0], std::stoi(parts[3])};
-    }
-
-}// namespace unnamed
+                <regsize ={1|2|4}>)"
+              << std::endl;
+    return res;
+}
+} // namespace
 
 namespace options
 {
@@ -117,63 +101,15 @@ int main(int argc, char *argv[])
         options::regsize != 4)
         return usage(prog_name, -1, "regsize allowed values: {1 | 2 | 4}");
 
-
-    auto [bps, data_bits, parity, stop_bits] = unpack_line_config (std::istringstream(options::line_config));
-
-    modbus_t *ctx = modbus_new_rtu(options::device.c_str(), bps, parity, data_bits, stop_bits);
-
-    if (!ctx)
-        throw std::runtime_error ("Failed creating ctx for device " + options::device);
-
-    int api_rv;
-    if (options::verbose)
-        api_rv = modbus_set_debug(ctx, TRUE);
-
-    api_rv = modbus_set_error_recovery(ctx,
-                              static_cast<modbus_error_recovery_mode>(
-                                    MODBUS_ERROR_RECOVERY_LINK |
-                                    MODBUS_ERROR_RECOVERY_PROTOCOL));
-
-    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(options::answering_time);
-    auto microseconds = std::chrono::microseconds(options::answering_time - seconds);
-
-    modbus_set_response_timeout(ctx, seconds.count(), microseconds.count());
-    modbus_set_slave(ctx, options::server_id);
-
-    if (modbus_connect(ctx) < 0)
-        throw std::runtime_error ("Failed modbus_connect: "s + modbus_strerror(errno));
+    auto serial_line = modbus::SerialLine(options::device, options::line_config);
+    modbus::RTUContext ctx(options::server_id, serial_line, options::answering_time, options::verbose);
 
     // if (options::address >= 40000)
     //     options::address -= 40000;
 
-    uint16_t regs[4];
-    api_rv = modbus_read_registers(ctx, options::address, options::regsize, regs);  // Holding register: Code 03
+    int64_t const val = ctx.read_holding_registers (options::address, options::regsize);
 
-    if (api_rv != options::regsize)
-        throw std::runtime_error ("Failed modbus_read_registers: "s + modbus_strerror(errno));
-
-    //api_rv = modbus_read_input_registers(ctx, options::address, options::regsize, regs);    // Input register: Code 04
-
-    int64_t val;
-    switch (options::regsize)
-    {
-        case 1:
-            val = regs[0];
-            break;
-        case 2:
-            val = MODBUS_GET_INT32_FROM_INT16(regs, 0);
-            break;
-        case 4:
-            val = MODBUS_GET_INT64_FROM_INT16(regs, 0);
-            break;
-        default:
-            assert (!"Unreachable");
-    }
-
-    std::cout << "API: " << api_rv << ", REGISTER: " << val << '\n';
-
-    modbus_close(ctx);
-    modbus_free(ctx);
+    std::cout << "REGISTER: " << val << '\n';
 
     return 0;
 }
