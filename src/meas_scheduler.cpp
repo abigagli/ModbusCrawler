a6ff1f067fdab78bf2c24e10e532bed9a3ce5ba2
Andrea Bigagli <abigagli@gmail.com>
2021-02-16 18:05:57 +0100

    Factor measure-specific operation in a dedicated lambda

--- src/meas_scheduler.cpp
+++ src/meas_scheduler.cpp
@@ -11,11 +11,7 @@ scheduler::add_schedule(modbus::RTUContext &modbus_cxt,
 {
     for (auto const &meas: measures)
     {
-#if defined(USE_ASIO_BASED_SCHEDULER)
-        impl_.addTask(
-          "Server_" + std::to_string(modbus_cxt.id()) + "/" + meas.name,
-          meas.sampling_period,
-          [this, &modbus_cxt, meas]() {
+        auto const meas_task = [this, &modbus_cxt, meas]() {
               if (true)
               {
                   auto const nowsecs =
@@ -49,49 +45,21 @@ scheduler::add_schedule(modbus::RTUContext &modbus_cxt,
               {
                   std::cerr << "******** FAILED *******\n";
               }
-          });
-#else
-        impl_.Schedule(
+          };
+
+#if defined(USE_ASIO_BASED_SCHEDULER)
+        impl_.addTask(
+          "Server_" + std::to_string(modbus_cxt.id()) + "/" + meas.name,
           meas.sampling_period,
-          modbus_cxt.id(),
-          [this, &modbus_cxt, meas](tsc::TaskContext tc) mutable {
-              if (true)
-              {
-                  auto const nowsecs =
-                    std::chrono::time_point_cast<std::chrono::seconds>(
-                      std::chrono::system_clock::now());
-
-                  std::cout << nowsecs.time_since_epoch().count() << ": "
-                            << modbus_cxt.name() << "@" << modbus_cxt.id()
-                            << ": reading register " << meas.source.address
-                            << "#" << meas.source.size << " repeat #"
-                            << tc.GetRepeatCounter() << ": ";
-              }
-
-              int64_t value;
-              try
-              {
-                  if (meas.source.type == modbus::regtype::holding)
-                      value = modbus_cxt.read_holding_registers(
-                        meas.source.address,
-                        meas.source.size,
-                        meas.source.endianess);
-                  else
-                      value =
-                        modbus_cxt.read_input_registers(meas.source.address,
-                                                        meas.source.size,
-                                                        meas.source.endianess);
-
-                  if (true)
-                      std::cout << value * meas.source.scale_factor << '\n';
-              }
-              catch (std::exception &e)
-              {
-                  std::cerr << "******** FAILED *******\n";
-              }
-
-              tc.Repeat();
-          });
+          meas_task
+          );
+#else
+        impl_.Schedule(meas.sampling_period,
+                       modbus_cxt.id(),
+                       [meas_task](tsc::TaskContext tc) {
+                           meas_task();
+                           tc.Repeat();
+                       });
 #endif
     }
 }
