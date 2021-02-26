1d20ae0acdc0d85483399b7580fb748c6bbc1d40
Andrea Bigagli <abigagli@gmail.com>
2021-02-26 16:56:34 +0100

    Add comments on why we use a map instead of a set to hold RTUContexts

--- src/meas_scheduler.h
+++ src/meas_scheduler.h
@@ -3,27 +3,33 @@
 #include "meas_config.h"
 #include "rtu_context.hpp"
 
-#if defined (USE_ASIO_BASED_SCHEDULER)
-#include "periodic_scheduler.h"
+#if defined(USE_ASIO_BASED_SCHEDULER)
+#    include "periodic_scheduler.h"
 #else
-#include <TaskScheduler.hpp>
-#include <thread>
+#    include <TaskScheduler.hpp>
+#    include <thread>
 #endif
 
 #include <chrono>
+#include <unordered_map>
 
 namespace measure {
 class scheduler
 {
-#if defined (USE_ASIO_BASED_SCHEDULER)
+#if defined(USE_ASIO_BASED_SCHEDULER)
     PeriodicScheduler impl_;
 #else
     tsc::TaskScheduler impl_;
 #endif
-    std::map<measure::server_id_t, modbus::RTUContext> mbcxts_;
+    // An unorderd_set would be the right choice, as we're not going to need to
+    // search through the container, but that would force const on all
+    // the elements and that doesn't work well with the lower level modbus C-api
+    // which works with (non-const) modbus_t *
+    std::unordered_map<measure::server_id_t, modbus::RTUContext> mbcxts_;
     bool verbose_;
 
-    void add_schedule (modbus::RTUContext &modbus_cxt, std::vector<measure_t> const &measures);
+    void add_schedule(modbus::RTUContext &modbus_cxt,
+                      std::vector<measure_t> const &measures);
 
 public:
     scheduler(configuration_map_t const &configmap, bool verbose = false)
@@ -34,9 +40,9 @@ public:
             auto const &server_config = el.second.server;
 
             auto cxt_insertion_result = mbcxts_.try_emplace(
-                //Key
+              // Key
               server_config.modbus_id,
-                //Args for RTUContext ctor
+              // Args for RTUContext ctor
               server_config.modbus_id,
               server_config.name,
               modbus::SerialLine(server_config.serial_device,
@@ -46,7 +52,9 @@ public:
 
 
             if (!cxt_insertion_result.second)
-                throw std::runtime_error("Failed creating RTUContext for modbus id " + std::to_string(server_config.modbus_id));
+                throw std::runtime_error(
+                  "Failed creating RTUContext for modbus id " +
+                  std::to_string(server_config.modbus_id));
 
             add_schedule(cxt_insertion_result.first->second,
                          el.second.measures);
